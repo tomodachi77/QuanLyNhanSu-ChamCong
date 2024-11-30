4b2421c96f0b4b7137cc7bf53d0c8979255ef3dd
unknown <khang.nguyenann@hcmut.edu.vn>
2024-11-30 10:49:36 +0700

    update

---
+++ trigger2.sql
@@ -0,0 +1,28 @@
+--Trigger để cập nhật tổng số giờ làm việc của nhân viên trong bảng BangChamCong khi có thay đổi trong bảng LanRaVao
+DELIMITER $$
+
+CREATE TRIGGER update_total_hours_after_insert
+AFTER INSERT ON LanRaVao
+FOR EACH ROW
+BEGIN
+    DECLARE total_hours DECIMAL(5, 2);
+    SET total_hours = TIMESTAMPDIFF(MINUTE, NEW.GioVao, NEW.GioRa) / 60;
+    
+    UPDATE BangChamCong
+    SET TongSoGioLam = COALESCE(TongSoGioLam, 0) + total_hours
+    WHERE MaNV = NEW.MaNV AND Ngay = NEW.Ngay;
+END$$
+
+CREATE TRIGGER update_total_hours_after_delete
+AFTER DELETE ON LanRaVao
+FOR EACH ROW
+BEGIN
+    DECLARE total_hours DECIMAL(5, 2);
+    SET total_hours = TIMESTAMPDIFF(MINUTE, OLD.GioVao, OLD.GioRa) / 60;
+    
+    UPDATE BangChamCong
+    SET TongSoGioLam = COALESCE(TongSoGioLam, 0) - total_hours
+    WHERE MaNV = OLD.MaNV AND Ngay = OLD.Ngay;
+END$$
+
+DELIMITER ;
