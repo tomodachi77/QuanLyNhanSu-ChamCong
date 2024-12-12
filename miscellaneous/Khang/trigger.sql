574ef8dae444af4bf9c23c828e28f9154abda65e
unknown <khang.nguyenann@hcmut.edu.vn>
2024-12-12 23:12:29 +0700

    cập nhật trigger update

--- miscellaneous/Khang/trigger.sql
+++ miscellaneous/Khang/trigger.sql
@@ -20,5 +20,23 @@ BEGIN
     SET SoLuongNhanVien = SoLuongNhanVien - 1
     WHERE MaPhongBan = OLD.MaPhongBan;
 END;
+DROP TRIGGER IF EXISTS update_employee_count_after_update;
+CREATE TRIGGER update_employee_count_after_update
+AFTER UPDATE ON NhanVien
+FOR EACH ROW
+BEGIN
+    -- Nếu phòng ban của nhân viên thay đổi, cập nhật số lượng nhân viên ở phòng ban cũ và mới
+    IF OLD.MaPhongBan != NEW.MaPhongBan THEN
+        -- Giảm số lượng nhân viên ở phòng ban cũ
+        UPDATE PhongBan
+        SET SoLuongNhanVien = SoLuongNhanVien - 1
+        WHERE MaPhongBan = OLD.MaPhongBan;
+        
+        -- Tăng số lượng nhân viên ở phòng ban mới
+        UPDATE PhongBan
+        SET SoLuongNhanVien = SoLuongNhanVien + 1
+        WHERE MaPhongBan = NEW.MaPhongBan;
+    END IF;
+END//
 
 DELIMITER ;
