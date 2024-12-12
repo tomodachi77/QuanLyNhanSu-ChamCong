c207b334b9bad7ad6929f5ee1e13cba766b24bcb
Quoc Khuong <khuonggm4869@gmail.com>
2024-12-13 00:51:26 +0700

    update PhongBanProcedure

--- initializeTable_Data/PhongBanProcedure.sql
+++ initializeTable_Data/PhongBanProcedure.sql
@@ -41,25 +41,4 @@ BEGIN
         EmployeeCount DESC;
 END//
 
---Tính tổng số giờ làm thêm của từng phòng ban trong một khoảng thời gian
-DROP PROCEDURE IF EXISTS TongGioLamThemTheoTungPhongBan//
-CREATE PROCEDURE TongGioLamThemTheoTungPhongBan(
-    IN p_NgayBatDau DATE,
-    IN p_NgayKetThuc DATE
-)
-BEGIN
-    SELECT 
-        pb.TenPhongBan,
-        SUM(TIMESTAMPDIFF(HOUR, lrv.GioVao, lrv.GioRa)) - COUNT(*) * 8 AS TongGioLamThem
-    FROM 
-        LanRaVao lrv
-    INNER JOIN NhanVien n ON lrv.MaNV = n.MaNV
-    INNER JOIN PhongBan pb ON n.MaPhongBan = pb.MaPhongBan
-    WHERE 
-        lrv.Ngay BETWEEN p_NgayBatDau AND p_NgayKetThuc
-    GROUP BY pb.TenPhongBan
-    HAVING SUM(TIMESTAMPDIFF(HOUR, lrv.GioVao, lrv.GioRa)) > COUNT(*) * 8
-    ORDER BY TongGioLamThem DESC;
-END //
-
 DELIMITER ;
